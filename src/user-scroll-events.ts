09a44d45620958c6330846538b1e7d1cb9fe4448
Ben Meyrick <bameyrick@gmail.com>
2021-05-04 15:31:53 +0100

    ES2015 module (#8)
    * Update deprecated code and now export as es2015 module
    
    * Lint on travis on push

--- src/user-scroll-events.ts
+++ src/user-scroll-events.ts
@@ -1,15 +1,3 @@
-import { EKeyCodes } from './keycodes';
-
 export const USER_SCROLL_EVENTS = ['mousedown', 'wheel', 'DOMMouseScroll', 'mousewheel', 'touchmove'];
 
-export const USER_SCROLL_KEYBOARD_EVENTS = [
-	EKeyCodes.SpaceBar,
-	EKeyCodes.PageUp,
-	EKeyCodes.PageDown,
-	EKeyCodes.End,
-	EKeyCodes.Home,
-	EKeyCodes.Left,
-	EKeyCodes.Up,
-	EKeyCodes.Right,
-	EKeyCodes.Down,
-];
+export const USER_SCROLL_KEYBOARD_EVENTS = [' ', 'PageUp', 'PageDown', 'End', 'Home', 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'];
